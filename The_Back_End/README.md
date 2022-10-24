781e899086b7358e9d4db352bba2800bffc59f6d
GaseousFob <73405660+GaseousFob@users.noreply.github.com>
2022-10-24 14:02:22 -0400

    Added gitignore and updated readme

--- The_Back_End/README.md
+++ The_Back_End/README.md
@@ -1,6 +1,10 @@
 CD into \The_Back_End\
-run 'venv\Scripts\activate'
-Then run these three commands
-set FLASK_APP=hello.py
-$env:FLASK_APP = "hello.py"
+Then run the following commands
+
+'Scripts\activate'
+
+set FLASK_APP=base.py
+
+$env:FLASK_APP = "base.py"
+
 flask run
--- The_Back_End/base.py
+++ The_Back_End/base.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import socket
 import json
 app = Flask(__name__)
 
@@ -47,3 +48,21 @@ def shoulderStop():
     # converting the payload to a json format for the response
     res = json.dumps(payload)
     return res
+
+
+# Below is the code given by Jacob as the sub to the pubsub
+# def client_program():
+#     host = 'ubuntu'  # as both code is running on same pc
+#     port = 5000  # socket server port number
+
+#     client_socket = socket.socket()  # instantiate
+#     client_socket.connect((host, port))  # connect to the server
+
+#     message = input(" -> ")  # take input
+
+#     while message.lower().strip() != 'bye':
+#         client_socket.send(message.encode())  # send message
+
+#         message = input(" -> ")  # again take input
+
+#     client_socket.close()  # close the connection

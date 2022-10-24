bb40a6333317cdc9fc7f1131409c672fdca1aee3
Thaxes <54343803+Thaxes@users.noreply.github.com>
2022-10-23 23:18:56 -0400

    Adding ROS2 packages
    Currently my custom package does not generate an executable, so I have kept the py_pubsub package and overrode the contents with my own code. I will add comments later this week.

---
+++ DMB/src/py_portserial/py_portserial/port_member_function.py
@@ -0,0 +1,88 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+import time
+from std_msgs.msg import String
+import socket
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'Shoulder', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 2
+        # get the hostname
+
+    host = socket.gethostname()
+    port = 5000  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # accept new connection
+
+
+    def timer_callback(self):
+
+        print("Connection from: " + str(self.address))
+        while True:
+            # receive data stream. it won't accept data packet greater than 1024 bytes
+            data = self.conn.recv(2).decode()
+            if not data:
+                # if data is not received break
+                break
+        msg = str(data)
+        self.publisher_.publish(msg)    
+        print("from connected user: " + msg)
+        self.get_logger().info('Publishing: "%s"' % msg)
+        #conn.send(data.encode())  # send data to the client
+
+        self.conn.close()  # close the connection        
+
+        #if (self.i == 2):
+        #   self.i = 1
+        #else:
+        #    self.i = 2
+        #msg = String()
+        #msg.data = str(self.i)
+        #self.publisher_.publish(msg)
+        #self.get_logger().info('Publishing: "%s"' % msg.data)
+        #time.sleep(1)
+        
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()

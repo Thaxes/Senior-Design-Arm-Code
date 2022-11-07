12dd7aadbc8c0e73b3c12b445fe576d9f6a791c6
Jacob Strayer <73405660+GaseousFob@users.noreply.github.com>
2022-11-07 14:04:19 -0500

    Encased connection attempt

--- The_Back_End/base.py
+++ The_Back_End/base.py
@@ -1,13 +1,23 @@
 from flask import Flask
+import time
 import socket
 import json
 app = Flask(__name__)
 
-host = '10.42.0.1'  # as both code is running on same pc
-port = 5000  # socket server port number
-
-client_socket = socket.socket()  # instantiate
-client_socket.connect((host, port))  # connect to the server
+for x in range(0, 3): # try 3 times, then stop trying to connect
+    try:
+        host = '10.42.0.1'  # as both code is running on same pc
+        port = 5000  # socket server port number
+        client_socket = socket.socket()  # instantiate
+        client_socket.connect((host, port))  # connect to the server
+    except Exception as err:
+        pass
+        # [WinError 10060] A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond
+    if err:
+        time.sleep(30) # sleep 30 seconds before attempting to reconnect
+        print('Looking for listener again...')
+    else: # otherwise, if we connected to the server, break this loop to allow for end point usage
+        break 
 
 
 def talker(res):
@@ -18,7 +28,7 @@ def talker(res):
 @app.route('/')
 def hello_world():
     payload = "me"
-    talker(payload)
+    talker(payload) # sending a message to the talker on the server we connected to above
     # converting the payload to a json format for the response
     res = json.dumps(payload)
     return res
